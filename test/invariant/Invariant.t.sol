1e687d3e54c488e136589740dcf265343e9645f5
mdasifahamed <mdasifahamed89@yahoo.com>
2024-02-06 12:52:01 +0600

    setUp Function Added

--- test/invariant/Invariant.t.sol
+++ test/invariant/Invariant.t.sol
@@ -13,5 +13,44 @@ import {MockERC20} from "../mocks/MOCKERC20.sol";
 
 
 contract Invariant is StdInvariant , Test {
+    // Pair Of Tokens For Pool
+    MockERC20 poolToken;
+    MockERC20 weth;
+
+    PoolFactory factory;
+    TSwapPool tswapPool;
+
+    // Initinal Token Balance 
+
+    int256 public constant STARTING_BALANCE_X = 100e18; // PoolToken Balance
+
+    int256 public constant STARTING_BALANCE_Y = 50e18; // WETHToken Balance
+
+    function setUp() public {
+        poolToken  = new MockERC20();
+        weth = new MockERC20();
+
+        factory = new PoolFactory(address(weth));
+        tswapPool =  TSwapPool(factory.createPool(address(poolToken)));
+
+        // Creating Ininitial Tokens For Kepping The Ratio
+
+        poolToken.mint(address(this), uint256(STARTING_BALANCE_X));
+        weth.mint(address(this), uint256(STARTING_BALANCE_Y));
+
+        // Approving Pool To Use These Token Pair 
+        poolToken.approve(address(tswapPool), type(uint256).max);
+        weth.approve(address(tswapPool), type(uint256).max);
+
+        tswapPool.deposit(
+            uint256(STARTING_BALANCE_Y),
+            uint256(STARTING_BALANCE_Y),
+            uint256(STARTING_BALANCE_X), 
+            uint64(block.timestamp));
+
+
+
+    }
+
 
 }
